977169bb76e4a30b7f817cc5c9b71777f35c692e
Unknown <lucien.barret@gmail.com>
2019-09-27 16:11:40 +0200

    hello

---
+++ implementations.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    return w, loss
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+def least_squares(y, tx):
+    return w, loss
+
+def ridge_regression(y, tx, lambda ):
+
+    return w, loss
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+
+    return w, loss
+
+def reg_logistic_regression(y, tx, lambda , initial_w, max_iters, gamma):
+
+    return w, loss
